64e54762c1014bfccfb87c5a798ecc86bb1215c7
Luis <iluisflores@yahoo.com>
2021-09-22 10:37:02 -0500

    wip: got the render function to work and now it displays the information from the API into the page

--- js/script.js
+++ js/script.js
@@ -1,29 +1,37 @@
 
 
 const BASE_URL = 'https://api.dictionaryapi.dev/api/v2'
+let def;
 
 const $input = $('input')
 const $form = $('form')
-let definition;
-const $definitionContainer = $('#definition-container')
+const $definitionContainer = $('.definition-container')
+const $exampleContainer = $('.example-container')
+const $originContainer = $('.origin-container')
 
 
 const handleGetData = event => {
     event.preventDefault()
     const query = $input.val()
     $.ajax(`${BASE_URL}/entries/en/${query}`)
-        .then(data => {
-            render()
-            definition = data
-            console.log(data)
-            console.log(query)
-        }), (err => {
-            console.log(err)
-        })
+    .then(data => {
+        def = data
+        render()
+        console.log(data)
+        console.log(query)
+    }), (err => {
+        console.log(err)
+    })
 }
 
 $form.on('submit', handleGetData)
 
 function render() {
-    $definitionContainer.text(definition);
+    $definitionContainer.text(`Definition: ${def[0].meanings[0].definitions[0].definition}`);
+    $exampleContainer.text(`Example: ${def[0].meanings[0].definitions[0].example}`);
+    $originContainer.text(`Origin: ${def[0].origin}`);
 }
+
+// function render() {
+//     $definitionContainer.text(def);
+// }
